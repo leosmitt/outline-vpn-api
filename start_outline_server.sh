935fdf58fd25d88ab17bb2bfcbbfee8104512819
leosmitt <64788702+leosmitt@users.noreply.github.com>
2025-11-28 07:03:19 +0630

    Update start_outline_server.sh

--- start_outline_server.sh
+++ start_outline_server.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 echo "Starting Outline Server installation..."
-sudo bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh)" > ./outline-install.log
+sudo bash -c "$(wget -qO- https://raw.githubusercontent.com/Jigsaw-Code/outline-server/master/src/server_manager/install_scripts/install_server.sh)"  ./outline-install.log
 echo "Outline Server installation completed."
 echo "Retrieving self-signed certificate..."
 sudo cp /opt/outline/persisted-state/shadowbox-selfsigned.crt .
